c785a52b0abd9d268014f985ea8d9aba0945c92a
Santi-Burgos <santiburgos38@gmail.com>
2026-05-03 16:51:31 -0300

    feat: little component

--- src/features/CustomerView/CustomerView.jsx
+++ src/features/CustomerView/CustomerView.jsx
@@ -1,7 +1,44 @@
+import styles from "./CustomerView.module.css";
+import { StatsCard } from "../../ui/StatsCard/StatsCard";
+import IconCustomer from "../../assets/customers.svg?react"
+import { CustomerTable } from "../../ui/CustomerTable/CustomerTable";
+
 export const CustomerView = () =>{
   return(
-    <div>
-      CUSTOMERS
-    </div>
+    <>
+      <section className={styles.containerHeader}>
+        <p>Admin / <span>Customer</span></p>
+        <button className={styles.addCustomer}>
+          Agregar Customer
+        </button>
+      </section>
+      <div className={styles.statsSection}> 
+        <div className={styles.cardContainer}>
+          <StatsCard
+            nameCard={"Total Customer"}
+            numberCard={100}
+            iconCard={IconCustomer}
+          />
+          <StatsCard
+            nameCard={"Total Customer"}
+            numberCard={100}
+            iconCard={IconCustomer}
+          />
+          <StatsCard
+            nameCard={"Total Customer"}
+            numberCard={100}
+            iconCard={IconCustomer}
+          />
+          <StatsCard
+            nameCard={"Total Customer"}
+            numberCard={100}
+            iconCard={IconCustomer}
+          />
+        </div>
+      </div>
+      <div>
+        <CustomerTable />
+      </div>
+    </>
   );
 }
--- src/ui/StatsCard/StatsCard.jsx
+++ src/ui/StatsCard/StatsCard.jsx
@@ -1,11 +1,15 @@
 import styles from "./StatsCard.module.css"
 
-export const StatsCard = ({nameCard, numberCard}) =>{
+export const StatsCard = ({nameCard, numberCard, iconCard: IconCard}) =>{
   return(
-    <div>
-      <div>
-        <p>{nameCard ? nameCard : "Name Card"}</p>
-        <p>{numberCard ? numberCard : "#"}</p>
+    <div className={styles.statsCardContainer}>
+      <div className={styles.statsBodyCard}>
+        <span>{nameCard ? nameCard : "Name Card"}</span>
+        <div className={styles.line}/>
+        <div className={styles.bodyItemsContainer}>
+          {IconCard && <IconCard />}
+          <p>{numberCard ? numberCard : "#"}</p>
+        </div>
       </div>
     </div>
   );
