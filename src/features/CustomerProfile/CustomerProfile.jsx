828a6bfcc6f644e8f0d66bc3f6b2580da781f6b5
Santi-Burgos <santiburgos38@gmail.com>
2026-05-09 17:39:26 -0300

    feat: resume/payment/ticket

--- src/features/CustomerProfile/CustomerProfile.jsx
+++ src/features/CustomerProfile/CustomerProfile.jsx
@@ -6,25 +6,30 @@ import TrashIcon from "../../assets/trashIcon.svg?react";
 import EditIcon from "../../assets/editIcon.svg?react";
 import PhoneIcon from "../../assets/phoneCustomer.svg?react";
 import EmailIcon from "../../assets/emailIcon.svg?react";
+import { StatsCard } from '../../ui/StatsCard/StatsCard';
+import MoneyBagIcon from "../../assets/moneyBagIcon.svg?react";
+import StatsIcon from "../../assets/statsAnalityc.svg?react";
+import TicketIcon from "../../assets/ticketIcon.svg?react";
 
 const CustomerProfile = () => {
   const { id } = useParams();
-  const [profileCustomer, setProfileCustomer ] = useState(null);
+  const [profileCustomer, setProfileCustomer] = useState(null);
+  const [activeCategory, setActiveCategory] = useState('resume');
 
-  useEffect(() =>{
-    const fetchData = async() =>{
-      try{
-        const data = await getSpecificCustomer({id})
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getSpecificCustomer({ id })
         console.log(data.data)
         setProfileCustomer(data?.data);
-      }catch(error){
+      } catch (error) {
         console.error("Error al traer la info del cliente:", error);
       }
     }
     fetchData();
   }, [])
 
-  if(!profileCustomer){
+  if (!profileCustomer) {
     return <div>cargando perfil...</div>
   }
 
@@ -56,12 +61,12 @@ const CustomerProfile = () => {
             <div className={styles.extraData}>
               <div className={styles.containerIndividualData}>
                 <div className={styles.containerIconData}>
-                  <PhoneIcon /> 
+                  <PhoneIcon />
                 </div>
                 <strong>{profileCustomer.customerPhone}</strong></div>
               <div className={styles.containerIndividualData}>
-                 <div className={styles.containerIconData}>
-                  <EmailIcon /> 
+                <div className={styles.containerIconData}>
+                  <EmailIcon />
                 </div>
                 <strong>{profileCustomer.customerEmail}</strong></div>
               <div>Creado: <strong>{profileCustomer.customerCreatedAt ? profileCustomer.customerCreatedAt : "####"}</strong></div>
@@ -84,23 +89,37 @@ const CustomerProfile = () => {
         </div>
       </div>
       <div className={styles.containerKPI}>
-        <div>
-          <div>
-            Title
-          </div>
-          <div>
-            Numero - Nombre
-          </div>
-        </div>
-        <div>
-          <div>
-            Title Payment
-          </div>
-          <div>
-            Numero - Nombre
-          </div>
-        </div>
+        <StatsCard />
+        <StatsCard />
+        <StatsCard />
       </div>
+      <section>
+        <div>
+          <div className={styles.categories}>
+            <div
+              className={`${styles.categorie} ${activeCategory === 'resume' ? styles.active : ''}`}
+              onClick={() => setActiveCategory('resume')}
+            >
+              <StatsIcon />
+              <h3>Resume</h3>
+            </div>
+            <div
+              className={`${styles.categorie} ${activeCategory === 'payments' ? styles.active : ''}`}
+              onClick={() => setActiveCategory('payments')}
+            >
+              <MoneyBagIcon />
+              <h3>Todos los pagos</h3>
+            </div>
+            <div
+              className={`${styles.categorie} ${activeCategory === 'tickets' ? styles.active : ''}`}
+              onClick={() => setActiveCategory('tickets')}
+            >
+              <TicketIcon />
+              <h3>Todos los tickets</h3>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
